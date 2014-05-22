4a073e2f2db84539936e36cf429deac27f0909d7
Louis Muhammad <louis.muhammad@gmail.com>
2014-05-22 10:09:35 -0400

    added helper method app/helpers/projects_helper.rb

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,9 @@
 module ProjectsHelper
+  def days_remaining(project)
+    if project.pledge_expired?
+      content_tag(:strong, 'All Done!')
+    else
+      distance_of_time_in_words(Time.now, project.pledging_ends_on) + " remaining" 
+    end
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ActiveRecord::Base
+  def pledge_expired?
+    pledging_ends_on.blank? || pledging_ends_on < Time.now
+  end
 end
